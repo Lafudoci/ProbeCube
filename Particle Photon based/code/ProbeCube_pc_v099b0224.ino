5565c3ed1d4c28c14104667ec6b57ebea1278525
Lafudoci <left228@hotmail.com>
2016-02-24 23:20:28 +0800

    Update ProbeCube_pc_v099b0224.ino

--- Particle Photon based/code/ProbeCube_pc_v099b0224.ino
+++ Particle Photon based/code/ProbeCube_pc_v099b0224.ino
@@ -1,37 +1,38 @@
 // This #include statement was automatically added by the Particle IDE.
-#include "blynk/blynk.h"
-#include "ThingSpeak/ThingSpeak.h"
 #include "PietteTech_DHT/PietteTech_DHT.h"
 
+// This #include statement was automatically added by the Particle IDE.
+#include "ThingSpeak/ThingSpeak.h"
 
-//thingspeak設定============================================================
+// This #include statement was automatically added by the Particle IDE.
+#include "blynk/blynk.h"
 
-unsigned long myChannelNumber = 267xx;
-const char * myWriteAPIKey = "Z9LVXARGKI6Kxxx"; //填入thingspeak API key ***必填
 
-//Blynk設定===================================================================
+//thingspeak設定
 
-char auth[] = "07dfd97a0c8c4927ba6a67126xxxxxx";
+unsigned long myChannelNumber = 26769; //填入thingspeak channel ID ***必填
+const char * myWriteAPIKey = "Z9LVXARGKI6KJJ1H"; //填入thingspeak API key ***必填
 
+//Blynk設定
+
+char auth[] = "07dfd97a0c8c4927ba6a67126fa7231c"; //填入blynk auth key ***必填
 
 //版本號
-#define VERSION "v1b0224"
+#define VERSION "v099b0224"
 
 //計時上傳
-unsigned long currentMillis = 0;
-unsigned long previousUpdateMillis = 0;
 int ThingspeakUpdateTime = 0;
 int BlynkUpdateTime = 0;
 #define Thingspeak_INTERVAL 60000
 #define Blynk_INTERVAL 5000
 
-//無線網路設定========================================================
+//無線網路設定
 
 TCPClient client;
 
-//SENSOR 設定==============================================================
+//SENSOR 設定
 //溫溼度
-#define DHTPIN 3 //指定要量測的腳位為D3
+#define DHTPIN 1 //指定要量測的腳位為D1
 #define DHTTYPE DHT22 //指定溫溼度感測器種類為DHT22
 char sensor_tem[5];
 char sensor_hum[5];
@@ -43,10 +44,8 @@ PietteTech_DHT DHT(DHTPIN, DHTTYPE, dht_wrapper);
 #define Compensator_tem 0
 #define Compensator_hum 0
 //===========================
-//土壤濕度
-#define soilSensor 2 //指定要量測的腳位為A2
 //有機汙染
-#define gasSensor 1 //指定要量測的腳位為A1
+#define gasSensor 0 //指定要量測的腳位為A0
 char sensor_voc[5];
 //懸浮微粒
 long  pmcf10 = 0;
@@ -56,25 +55,15 @@ long  pmat10 = 0;
 long  pmat25 = 0;
 long  pmat100 = 0;
 char buf[50];
-//指定指示燈腳位
-#define red_led     7
-#define yellow_led  8
-#define green_led   9
-//****************************************************************************
+
+//***************************************************
 
 void setup(void)     //初始化函數
 {
-  Particle.publish("Start version" , VERSION);
+  Particle.publish("Start ver." , VERSION);
 
   Serial.begin(115200);
 
-  //LED們初始化
-  pinMode(red_led, OUTPUT);
-  pinMode(yellow_led, OUTPUT);
-  pinMode(green_led, OUTPUT);
-
-  LED_start();
-  LED_check();
 
   //懸浮微粒感測器初始化
   Serial1.begin(9600);
@@ -82,11 +71,8 @@ void setup(void)     //初始化函數
   //blynk
   Blynk.begin(auth);
 
-  //ThingSpeak方法初始化================================================================
-
+  //ThingSpeak方法初始化
   ThingSpeak.begin(client);
-
-  LED_ok();
 
   delay(500);       //延遲500ms
 
@@ -143,9 +129,6 @@ void loop(void)     //循環函數區域
   sprintf(sensor_tem, "%.1f", measure_tem());
   sprintf(sensor_hum, "%.1f", measure_hum());
 
-  //讀取土壤濕度
-  int sensor_soil = measure_soil();
-
   //讀取有機氣體汙染
   sprintf(sensor_voc, "%.1f", measure_voc());
 
@@ -154,23 +137,17 @@ void loop(void)     //循環函數區域
   measure_dust();
   int sensor_dust25 = pmat25;
   int sensor_dust100 = pmat100;
-  //產生亂數
-  int ran = random(100, 999);
 
 
-
-  //將數據上傳 ======================================================
-
+  //將數據上傳
 
   //使用thingspeak方法送出數據
   if (millis() > ThingspeakUpdateTime) {
     ThingSpeak.setField(1, sensor_tem);
     ThingSpeak.setField(2, sensor_hum);
-    ThingSpeak.setField(3, sensor_soil);
     ThingSpeak.setField(4, sensor_voc);
     ThingSpeak.setField(5, sensor_dust25);
     ThingSpeak.setField(6, sensor_dust100);
-    ThingSpeak.setField(8, ran);
     ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
 
     Serial.println(F("Data has been sent to Thingspeak !!")); //serial & particle回報狀態訊息
@@ -182,7 +159,6 @@ void loop(void)     //循環函數區域
   if (millis() > BlynkUpdateTime) {
     Blynk.virtualWrite(V1, sensor_tem);
     Blynk.virtualWrite(V2, sensor_hum);
-    Blynk.virtualWrite(V3, sensor_soil);
     Blynk.virtualWrite(V4, sensor_voc);
     Blynk.virtualWrite(V5, sensor_dust25);
     Blynk.virtualWrite(V6, sensor_dust100);
@@ -193,17 +169,13 @@ void loop(void)     //循環函數區域
 
 
 }
-//方法呼叫區******************************************************************
+//方法呼叫區*********************************************
 float measure_tem() {
   return (DHT.getCelsius() + Compensator_tem);
 }
 
 float measure_hum() {
   return (DHT.getHumidity() + Compensator_hum);
-}
-
-float measure_soil() {
-  return analogRead(soilSensor);
 }
 
 float measure_voc() {
@@ -283,86 +255,3 @@ void measure_dust() {
   return;
 }
 
-void LED_start()
-{
-  digitalWrite(red_led, HIGH);
-  digitalWrite(yellow_led, HIGH);
-  digitalWrite(green_led, HIGH);
-  delay(1000);
-  digitalWrite(red_led, LOW);
-  digitalWrite(yellow_led, LOW);
-  digitalWrite(green_led, LOW);
-  delay(500);
-  digitalWrite(red_led, HIGH);
-  digitalWrite(yellow_led, HIGH);
-  digitalWrite(green_led, HIGH);
-  delay(1000);
-  digitalWrite(red_led, LOW);
-  digitalWrite(yellow_led, LOW);
-  digitalWrite(green_led, LOW);
-}
-
-void LED_check()
-{
-  digitalWrite(red_led, HIGH);
-  digitalWrite(yellow_led, HIGH);
-  digitalWrite(green_led, LOW);
-}
-
-void LED_wait()
-{
-  digitalWrite(red_led, LOW);
-  digitalWrite(yellow_led, HIGH);
-  digitalWrite(green_led, LOW);
-}
-
-void LED_error()
-{
-  digitalWrite(red_led, HIGH);
-  digitalWrite(yellow_led, LOW);
-  digitalWrite(green_led, LOW);
-}
-
-void LED_ok()
-{
-  digitalWrite(red_led, LOW);
-  digitalWrite(yellow_led, LOW);
-  digitalWrite(green_led, HIGH);
-}
-
-void LED_send()
-{
-  digitalWrite(green_led, LOW);
-  delay(100);
-  digitalWrite(green_led, HIGH);
-  delay(100);
-  digitalWrite(green_led, LOW);
-  delay(100);
-  digitalWrite(green_led, HIGH);
-  delay(100);
-  digitalWrite(green_led, LOW);
-  delay(100);
-  digitalWrite(green_led, HIGH);
-}
-
-void LED_warning_yellow()
-{
-  for (int i = 0; i <= 270; i++) {
-    digitalWrite(yellow_led, HIGH);
-    delay(100);
-    digitalWrite(yellow_led, LOW);
-    delay(100);
-  }
-}
-
-void LED_warning_red()
-{
-  for (int i = 0; i <= 270; i++) {
-    digitalWrite(red_led, HIGH);
-    digitalWrite(yellow_led, HIGH);
-    delay(100);
-    digitalWrite(red_led, LOW);
-    digitalWrite(yellow_led, LOW);
-    delay(100);
-  }
-}
